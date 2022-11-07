26a640e5dfa993abdfcc22a2e1b583a3c9fed3f7
Daisy Le <daisy.le0203@gmail.com>
2022-11-07 17:11:26 -0600

    added comments in the connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,15 @@
+// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
 let sequelize;
 
+// when deployed on Heroku
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+  // localhost
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
