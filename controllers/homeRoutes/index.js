63d2cbb53df8784219e1598f6e82d24eadabefba
Daisy Le <daisy.le0203@gmail.com>
2022-11-07 17:12:55 -0600

    set up all the endpoints and routes

--- controllers/homeRoutes/index.js
+++ controllers/homeRoutes/index.js
@@ -1,6 +1,14 @@
+// This contains all of the user-facing routes, such as the homepage, login ans signup page
 const router = require('express').Router();
+const sequelize = require('../config/connection');
 
+// rendering all posts to homepage
 
+// redirecting users to homepage once they log in
+
+// rendering sign up page 
+
+//rendering one post to the single-post page
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
 //   if (req.session.logged_in) {
--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,12 @@
+// index.js for all controllers
 const router = require('express').Router();
 
+const apiRoutes = require("./api")
 const homeRoutes = require('./homeRoutes');
+const dashboardRoutes = require ("./dashboardRoutes");
 
+router.use("/api", apiRoutes)
 router.use('/', homeRoutes);
+router.use("/dashboard", dashboardRoutes)
 
 module.exports = router;
