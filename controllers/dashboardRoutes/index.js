d2b112d99798a83dbe97c187863b9575e8843a63
Daisy Le <daisy.le0203@gmail.com>
2022-11-10 16:33:54 -0600

    finished the dashboard routes

--- controllers/dashboardRoutes/index.js
+++ controllers/dashboardRoutes/index.js
@@ -1,20 +1,96 @@
-const router = require('express').Router();
-const { Post, User, Comment } = require('../../models');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router()
+const sequelize = require("../../config/connection")
+const { Post, User, Comment } = require("../../models")
+const withAuth = require("../../utils/auth")
 
-// dashboard displaying posts created by logged in users 
-router.get('/', (req, res) => {
-    res.render('dashboard');
-  });
+// dashboard displaying posts created by logged in users
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
+      attributes: ["id", "title", "post_text", "created_at"],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "user_id",
+            "post_id",
+            "created_at",
+          ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    })
 
-// rendering edit page
-router.get('/edit/:id', (req, res) => {
-    res.render('edit-posts');
-  });
+    const posts = postData.map((post) => post.get({ plain: true }))
 
-// rendering new post page 
-router.get('/newpost', (req, res) => {
-    res.render('new-posts');
-  });
+    res.render("dashboard", {
+      posts,
+      logged_in: true,
+      username: req.session.username,
+    })
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
 
-module.exports = router;
+// rendering edit post page by the user's id
+router.get("/edit/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ["id", "title", "post_text", "created_at"],
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "user_id",
+            "post_id",
+            "created_at",
+          ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+      ],
+    })
+
+    const post = postData.get({ plain: true })
+
+    res.render("edit-post", {
+      post,
+      logged_in: true,
+      username: req.session.username,
+    })
+  } catch (error) {
+    console.log(err)
+    res.status(500).json(error)
+  }
+})
+
+// rendering new post page
+router.get("/newpost", (req, res) => {
+  res.render("new-post", { username: req.session.username })
+})
+
+module.exports = router
